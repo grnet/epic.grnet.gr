662134fef32837bddd2f3a73f2880d72adcf8064
zamani <zamani@cytech.gr>
2014-02-17 11:46:27 +0200

    Code in Tabs v 2.0

--- lib/tabs.rb
+++ lib/tabs.rb
@@ -3,9 +3,11 @@ module TabsHelper
   def tabs
     @items.select { |item| item[:kind] == 'code' }
   end
+
   def render_child(name)
-    @items.select { |item| item[:parent] == name }
+    @items.select { |item| item[:parent] == name }.sort_by{ |item| item[:order] }
   end
+
 end
 
 include TabsHelper
