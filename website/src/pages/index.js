db9d43095e4f2abb335f7dd275bfe5dcfc3ca643
Tas-sos <tasosnumberten@yahoo.gr>
2021-03-31 12:07:23 +0300

    Docusaurus v2.

---
+++ website/src/pages/index.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: 'Get Started',
+    imageUrl: 'img/undraw_annotation.svg',
+    description: (
+      <>
+        Get started with the ePIC API.
+        Browse the documentation
+      </>
+    ),
+  },
+  {
+    title: 'API',
+    imageUrl: 'img/undraw_code_typing.svg',
+    description: (
+      <>
+        Enable the use of persistent identifiers in your application using the ePIC API
+      </>
+    ),
+  },
+  {
+    title: 'Contact',
+    imageUrl: 'img/undraw_contact_us.svg',
+    description: (
+      <>
+        Are you interested in using Persistent Identifiers for your data set? Get in touch with us to get you started. Just fill out the <a href="https://docs.google.com/forms/d/e/1FAIpQLSf0UqSzLQfOJvk0qtXWNibL4WKUiDaNKy6POTmGhxXcNrlSzw/viewform"> form </a>.
+      </>
+    ),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/')}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map((props, idx) => (
+                  <Feature key={idx} {...props} />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
+
+export default Home;
