860a868356cd3f8c40046171128eabade9bee7a2
Themis Zamani <themiszamani@gmail.com>
2023-05-17 14:47:38 +0300

    Update sidebars.js

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   someSidebar: {
     Glossary: ['glossary'],
+    'ΑPI support': ['api-overview','api-account'],
     'EPIC API': ['overview','api', 'api-auth', 'api-generation', 'api-get', 'api-list', 'api-search', 'api-create', 'api-update', 'api-delete', 'api-partial' ],
     'Handle API v9.0': ['handle', 'api-calls', 'requests', 'cross-origin', 'responses', 'methods' ]
     //Docusaurus: ['doc1', 'doc2', 'doc3'],
